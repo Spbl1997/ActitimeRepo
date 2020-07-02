8589b9f7981a170660e4a58b59e85f0c43c227e7
DELL <DELL@DESKTOP-FM58BBI>
2020-07-02 14:46:44 +0530

    my first commit

---
+++ actiTIMEMVN/src/test/java/com/actitme/login/Demo.java
@@ -0,0 +1,11 @@
+package com.actitme.login;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo {
+@Test
+public void run() {
+	Reporter.log("Build Is Sucessfully created.",true);
+}
+}
