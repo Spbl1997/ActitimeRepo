55d43162dad875adaac535ef2bbc4dec730e411b
DELL <DELL@DESKTOP-FM58BBI>
2020-07-02 15:11:16 +0530

    Second commit

--- actiTIMEMVN/src/test/java/com/actitme/login/Demo.java
+++ actiTIMEMVN/src/test/java/com/actitme/login/Demo.java
@@ -2,7 +2,11 @@ package com.actitme.login;
 
 import org.testng.Reporter;
 import org.testng.annotations.Test;
-
+/**
+ *  Test Script
+ * @author DELL
+ *
+ */
 public class Demo {
 @Test
 public void run() {
